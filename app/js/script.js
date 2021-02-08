f569812a9dfce7a51f1af60dc3b959fb1daaa1db
cmroonn <fallgetell@gmail.com>
2021-02-08 11:35:52 +0500

    commit

--- app/js/script.js
+++ app/js/script.js
@@ -50,17 +50,34 @@ $(document).ready(function() {
     $('.restaurant-schema__place').click(function() {
         var place = $(this).attr('data-place');
         $(this).toggleClass('clicked');
+
         if ($(this).hasClass('clicked')) {
             placesArray.push($(this).attr('data-place'));
             document.getElementById('ordered').innerHTML += `
-                <span data-choosen='${placesArray[placesArray.length - 1]}'>${placesArray[placesArray.length - 1]},</span>
+                <span data-choosen='${placesArray[placesArray.length - 1]}' class="chosen-place">${placesArray[placesArray.length - 1]},</span>
                 `;
 
             document.querySelector('.book__content__form__content__order .jspPane').innerHTML += `
-                <p data-choosen="${place}">Столик №${placesArray[placesArray.length - 1]}</p>
+                <p data-choosen="${place}" class="chosen-place">Столик №${placesArray[placesArray.length - 1]}</p>
              `;
 
             $('.book__content__form__content__order').jScrollPane();
+
+            console.log(document.querySelectorAll('.chosen-place'));
+            document.querySelectorAll('.chosen-place').forEach(function(el) {
+                el.addEventListener('click', function() {
+                    console.log(el.dataset.choosen);
+                    placesArray = $.grep(placesArray, function(value) {
+                        return value != el.dataset.choosen;
+                    });
+
+                    document.getElementById('ordered').querySelector(`span[data-choosen='${el.dataset.choosen}']`).remove();
+                    document.querySelector(`.book__content__form__content__order p[data-choosen="${el.dataset.choosen}"`).remove();
+                    $('.book__content__form__content__order').jScrollPane();
+                    $(`.restaurant-schema__place[data-place=${el.dataset.choosen}]`).toggleClass('clicked');
+                });
+            });
+
 
         } else {
             placesArray = $.grep(placesArray, function(value) {
@@ -70,6 +87,32 @@ $(document).ready(function() {
             document.querySelector(`.book__content__form__content__order p[data-choosen="${place}"`).remove();
             $('.book__content__form__content__order').jScrollPane();
         }
+    });
+
+
+    $('#clearAll').click(function() {
+        placesArray = [];
+        $('.chosen-place').remove();
+        $('.restaurant-schema__place').removeClass('clicked');
+
+    });
+
+
+    $('.masked-input').mask("+7(999) 999-99-99");
+
+    $('.form-name-and-phone').validate({
+        rules: {
+            phone: {
+                required: true,
+                minlength: 17,
+            },
+        },
+        messages: {
+            phone: {
+                required: 'Пожалуйста, введите свой номер телефона',
+            }
+        }
+
     });
 
 });
